938aa9a04e4eec745a483c142556a0d4f39a69f5
Emmanuel Ibarra V. <Fran-RivasWNER@outlook.es>
2024-05-30 22:16:13 -0600

    metodo fill y font

--- js/game.js
+++ js/game.js
@@ -7,6 +7,11 @@ const game = canvas.getContext('2d')
 window.addEventListener('load', startGame)
 
 function startGame(){
-    game.fillRect(0,0,100,100)
-    game.clearRect(0,0,50,50)
+/*     game.fillRect(0,50,100,100)
+    game.clearRect(0,0,50,50) */
+
+
+    game.fillFont = '25px Verdana'
+    game.fillStyle = 'purple'
+    game.fillText('XXX', 100,100)
 }
