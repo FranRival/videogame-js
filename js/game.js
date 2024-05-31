4d3cc7fa27ca37828f4a46fe53d1b90183cca184
Emmanuel Ibarra V. <Fran-RivasWNER@outlook.es>
2024-05-30 22:07:12 -0600

    fillRect, y clearRect

---
+++ js/game.js
@@ -0,0 +1,12 @@
+
+//canvas en js
+
+const canvas = document.querySelector('#game')
+const game = canvas.getContext('2d')
+
+window.addEventListener('load', startGame)
+
+function startGame(){
+    game.fillRect(0,0,100,100)
+    game.clearRect(0,0,50,50)
+}
