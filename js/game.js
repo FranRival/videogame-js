6e7cd03b48ac7aec565ff594f33bfb51f48a1aaf
Emmanuel Ibarra V. <Fran-RivasWNER@outlook.es>
2024-05-31 22:31:09 -0600

    tratando de acomodar la bomba en el eje 0 0

--- js/game.js
+++ js/game.js
@@ -24,6 +24,15 @@ function startGame(){
 
     const elementsSize = canvasSize / 10
     console.log({canvasSize, elementsSize});
+
+    game.font= elementsSize + 'px Verdana'
+
+    for (let i = 0; i < 10; i++) {
+        const element = 10[i];
+        game.fillText(emojis['X'], 0 * i, 0 * i)
+    }
+
+
 }
 
 
