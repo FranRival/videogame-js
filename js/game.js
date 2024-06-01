3670491f7618f0589c80cfdf7cdf24eb23677bc3
Emmanuel Ibarra V. <Fran-RivasWNER@outlook.es>
2024-05-31 21:56:18 -0600

    setAtributte para la pantalla

--- js/game.js
+++ js/game.js
@@ -7,11 +7,20 @@ const game = canvas.getContext('2d')
 window.addEventListener('load', startGame)
 
 function startGame(){
+
+    let canvasSize
+    canvas.setAttribute('width', canvasSize * .75)
+    canvas.setAttribute('width', canvasSize * .5)
+
+    
 /*     game.fillRect(0,50,100,100)
     game.clearRect(0,0,50,50) */
 
 
-    game.fillFont = '25px Verdana'
+/*     game.fillFont = '25px Verdana'
     game.fillStyle = 'purple'
-    game.fillText('XXX', 100,100)
+    game.fillText('XXX', 100,100) */
 }
+
+
+//tamano del canvas y sus elementos
