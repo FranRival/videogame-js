026f17597806903a8ff7e8d2e01c17f86a917e8e
Emmanuel Ibarra V. <Fran-RivasWNER@outlook.es>
2024-06-15 21:41:48 -0600

    this is puta magia. ahora funciona este array alv. la question ahora es: como dickenson emojis['HEART'] esta ligado a la modofoko de vidas?

--- js/game.js
+++ js/game.js
@@ -86,7 +86,8 @@ function startGame(){
 
 function showLives(){
     spanLives.innerHTML = emojis['HEART']
-    Array(lives) //crear un array. no sabemos que tipo de elemento. string. o numero. o true. o false.
+    const heartArray = Array(lives).fill(emojis['HEART'])//crear un array. no sabemos que tipo de elemento. string. o numero. o true. o false.
+    console.log(heartArray);
 }
 
 function levelFail(){
