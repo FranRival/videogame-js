3102f9bd6bfe43980898fb6945cf791b4b304e76
Emmanuel Ibarra V. <Fran-RivasWNER@outlook.es>
2025-10-20 21:51:32 -0600

    descompusimos el ciclo for anidado.

--- js/game.js
+++ js/game.js
@@ -34,16 +34,43 @@ function startGame(){
     //96px Verdana
 
     for (let i = 0; i < 10; i++) {
+        //10[1] = undefined.
         const element = 10[i];
         console.log(element);
         console.log(i);
-        //emojis[X], game.js tiene acceso a maps.js. porque se definio asi en el html
 
 
         //aqui se dibuja 💣 en la pocision 0, 0
         game.fillText(emojis['X'], 0 * i, 0 * i)
 
     }
+
+
+
+
+    ///CHATGPT
+
+    const mapa = maps[0].trim().split('\n');
+    //transformado en array. 9 bloques.
+
+    for (let y = 0; y < mapa.length; y++) {
+        //y = 1, 2, 3, 4...9
+
+        //este for modifica fila en cada iteracion
+
+
+        //mutable. fila = mapa[0],[1],[2]...[9]
+    const fila = mapa[y];
+    //mapa[1, 2, 3, 4...9]
+    for (let x = 0; x < fila.length; x++) {
+
+        //este for usa fila para acceder a sus caracteres
+
+        const letra = fila[x];       // 'I', 'X', '-', 'O'
+        const emoji = emojis[letra]; // convierte letra a emoji
+        game.fillText(emoji, x*elementsSize, y*elementsSize);
+    }
+}
 
 
 
