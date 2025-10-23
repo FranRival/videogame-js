02c75a929a2363ab528be1c46226f6eca8bbc1e7
Emmanuel Ibarra V. <Fran-RivasWNER@outlook.es>
2025-10-22 20:23:48 -0600

    volviendo a descomponer el for.

--- js/game.js
+++ js/game.js
@@ -29,7 +29,7 @@ function startGame(){
     for (let i = 0; i < 10; i++) {
         const element = 10[i];
         console.log(element);
-        console.log(i);
+        console.log('el valor de i ' + i);
 
         game.fillText(emojis['X'], 0 * i, 0 * i)
 
@@ -43,10 +43,12 @@ function startGame(){
     const mapa = maps[0].trim().split('\n');
 
     for (let y = 0; y < mapa.length; y++) {
-    const fila = mapa[y];
+    const fila = mapa[y].trim();
     for (let x = 0; x < fila.length; x++) {
-        const letra = fila[x];       
-        const emoji = emojis[letra]; 
+        const letra = fila[x];
+        //X, no guarda el indice
+        const emoji = emojis[letra];
+        //traduce X, I o - 💣, 🎁
         game.fillText(emoji, x*elementsSize, y*elementsSize);
     }
 }
